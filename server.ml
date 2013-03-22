2dc0c15eb294511211e88ef2f448497fe2afc8a2
nevergone-sc <flycookie88@gmail.com>
2013-03-22 01:47:18 +0000

    further presence & message

--- server.ml
+++ server.ml
@@ -23,10 +23,14 @@ class conn_info
 	?p_init:(p_i="") () =
     object
         val mutable outchan = o_i
-        val mutable rosters = r_i
         val mutable presence = p_i
 		method get_outchan = outchan
-        method change_outchan o = outchan <- o;
+        method change_outchan o = outchan <- o
+		method get_presence = presence
+		method change_presence p = presence <- p
+        (*val mutable rosters = r_i
+		method get_roster = rosters
+		method change_roster rs = rosters <- rs*)
     end;;
 
 class service conn_init =
@@ -40,6 +44,7 @@ class service conn_init =
 		val mutable stream_id = ""
 		val mutable state = Closed
 		val mutable contacts = []
+		val mutable contacts_temp = []
 		val mutable proceed = false
 		val mutable level1 = (("",""), [])
 		val mutable level2 = (("",""), [])
@@ -100,6 +105,7 @@ class service conn_init =
 
 		method start_handler = function
 			| Start_element ((ns, name), att)  -> 
+				print_string "here!!";
 				stream_id <- begin try
                 		UTF8.encode (List.assoc ([], [105; 100]) att)
                 	with Not_found -> this#gen_id
@@ -161,7 +167,7 @@ class service conn_init =
 					else if level1_tp = "set" then
 						if level2_ns = "jabber:iq:auth" then
 							(* TODO:consider usage of <digest>, <resource> *)
-							let username = try (UTF8.encode (List.assoc "username" contents))
+							let username = try (UTF8.encode (snd (List.assoc ("", "username") contents)))
 							 			   with Not_found -> "" in
 							let conn_inst = new conn_info ~o_init:(of_fd output connection) () in
 								client_id <- (username ^ "@" ^ my_name);
@@ -176,7 +182,7 @@ class service conn_init =
 				level3 <- ((UTF8.encode ns, UTF8.encode name), att);
 			| End_element (ns, name) when xml_parser#level = 3 ->
 				let ((_, n), _) = level3 in 
-					contents <- (n, chardata) :: contents;
+					contents <- (("", n), ([],chardata)) :: contents;
 					chardata <- []
 			| End_element _ when xml_parser#level > 1 ->
 				chardata <- []
@@ -193,48 +199,87 @@ class service conn_init =
 				let ((_, _), att) = level1 in
 				let ((level2_ns, _), _) = level2 in
 				let level1_tp = try (UTF8.encode (List.assoc ([], UTF8.decode "type") att)) with Not_found -> ""
-				and level1_id = try (UTF8.encode (List.assoc ([], UTF8.decode "id") att))
-							 	with Not_found -> "" (* TODO:should raise exception here*) in
+				and level1_id = try (UTF8.encode (List.assoc ([], UTF8.decode "id") att)) with Not_found -> ""
+				and level1_la = try (UTF8.encode (List.assoc xml_lang att)) with Not_found -> ""
+				and level1_to = try (UTF8.encode (List.assoc ([], UTF8.decode "to") att)) with Not_found -> "" in
 					if name = UTF8.decode "iq" then
-						if level1_tp = "get" then
-							if level2_ns = "jabber:iq:roster" then begin
-								print_string client_id;
-								contacts <- !(Hashtbl.find roster client_id);
-								let str_iq = ref ("<iq to='" ^ client_id ^ "' type='result' id='" ^ level1_id ^ "'>
-													<query xmlns='jabber:iq:roster'>") in
-	    	  		 			let list_iter (jid, name, subs, groups) =
-			                    	let rec str_groups = function
-			                        	| []   -> ""
-			                        	| g :: gs -> ("<group>" ^ g ^ "</group>" ^ (str_groups gs))
-			                    	in
-			                    	str_iq := (!str_iq ^ "<item jid='" ^ jid ^
-												"' name='" ^ name ^
-												"' subscription='" ^ subs ^ "'>" ^
-												str_groups groups ^ "</item>")
-								in
-			                	    List.iter list_iter contacts;
-									this#send (!str_iq ^ "</query></iq>");
-								end
-							else ()
+						if level1_tp = "get" && level2_ns = "jabber:iq:roster" then
+							begin
+							print_string client_id;
+							contacts <- !(Hashtbl.find roster client_id);
+							let str_iq = ref ("<iq to='"^client_id^"' type='result' id='"^level1_id^"'>
+												<query xmlns='jabber:iq:roster'>") in
+	      		 			let list_iter (jid, name, subs, groups) =
+		                    	let rec str_groups = function
+		                        	| []   -> ""
+		                        	| g :: gs -> ("<group>" ^ g ^ "</group>" ^ (str_groups gs))
+		                    	in
+		                   		str_iq := (!str_iq ^ "<item jid='" ^ jid ^ "' name='" ^ name ^
+											"' subscription='" ^ subs ^ "'>" ^ str_groups groups ^ "</item>")
+							in
+		                	    List.iter list_iter contacts;
+								this#send (!str_iq ^ "</query></iq>");
+							end
 						else ()
 					else if name = UTF8.decode "presence" then
-					(*TODO: send to presence available only *)
-					(*TODO: add to global_info *)
-						begin
-						let broadcast (id, _, subs, _) =
-							if subs = "both" || subs = "from" then
-								this#send_to id ("<presence xmlns='jabber:client' to='" ^ id ^ "' from='"
-												 ^ client_id  ^ "'>" ^ stanza_temp ^ "</presence>");
-						in
-						List.iter broadcast contacts;
-						stanza_temp <- ""
-						end
+						(* probe presence *)
+						(*TODO: may not implement server-server interaction *)
+						if level1_tp = "probe" then ()
+						(* initial, subsequent and unavailable presences *)
+						else 
+							(*TODO: send to all resources in same account *)
+							if level1_to = "" then
+								begin
+								let broadcast (id, _, subs, _) =
+									if (subs = "both" || subs = "from") && (Hashtbl.mem global_info id) then
+										this#send_to id ("<presence xmlns='jabber:client' to='" ^ id ^ "' from='"                               				  ^client_id^"' type='"^level1_tp^"'>"^stanza_temp^"</presence>")
+								in
+								List.iter broadcast contacts;
+								(* send to temporary contacts *)
+								if level1_tp = "unavailable" then begin 
+									List.iter broadcast contacts_temp;
+									contacts_temp <- []
+									end;
+								stanza_temp <- "";
+								(* update to global_info *)
+								let conn_inst = Hashtbl.find global_info client_id in
+									conn_inst#change_presence stanza_temp
+								end
+							(* if it is a directed presence *)
+							else
+								try 
+								this#send_to level1_to ("<presence xmlns='jabber:client' to='" ^ level1_to ^ "' from='"^client_id^"' type='"^level1_tp^"'>"^stanza_temp^"</presence>");
+								stanza_temp <- "";
+								contacts_temp <- (level1_to, "", "", []) :: contacts_temp
+								with Not_found -> () (*TODO: raise error *)
+					else if name = UTF8.decode "message" then
+						if level1_tp = "chat" then begin
+							try this#send_to level1_to ("<message from='" ^ client_id  ^ "' id='" ^ level1_id  ^ 
+														"' to='"^level1_to^"' type='chat' xml:lang='"^level1_la ^
+														"'>" ^ stanza_temp  ^ "</message>")
+							with Not_found -> (); (*TODO: raise exception *)
+							print_string ("<message from='" ^ client_id  ^ "' id='" ^ level1_id  ^ 
+														"' to='"^level1_to^"' type='chat' xml:lang='"^level1_la ^
+														"'>" ^ stanza_temp  ^ "</message>");
+							Pervasives.flush Pervasives.stdout;
+							contents <- [];
+							stanza_temp <- ""
+							end
+
 			| End_element (ns, name) when xml_parser#level = 2 ->
 				let ((_, level1_name), _) = level1 in
-				let ((level2_ns, level2_name), level2_att) = level2 in
+				let ((_, level2_name), level2_att) = level2 in
+				let level2_la = try (UTF8.encode (List.assoc xml_lang level2_att)) with Not_found -> "" in
 				if level1_name = "presence" then 
 					stanza_temp <- (stanza_temp ^ xml_parser#raw_string)
-				else ()
+				else if level1_name = "message" then
+					(* support multiple <body> stanza with distinct xml:lang *)
+					if (List.mem_assoc (level2_la, level2_name) contents) then 
+						() (*TODO: raise conflict exception *)
+					else begin
+						contents <- ((level2_la, level2_name), ([], [])) :: contents;
+						stanza_temp <- (stanza_temp ^ xml_parser#raw_string)
+						end
 			| CharData s when xml_parser#level = 2 -> 
 				let ((_, level1_name), _) = level1 in
 				if level1_name = "presence" then chardata <- s
